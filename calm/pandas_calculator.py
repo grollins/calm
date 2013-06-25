3815acac881ffebfde62ae83da7271a4bbe59760
Geoff Rollins <grollins@gmail.com>
2013-06-25 13:22:29 -0700

    Implement calculator methods for mean, median, absolute and square differences, and min value.

--- calm/pandas_calculator.py
+++ calm/pandas_calculator.py
@@ -4,4 +4,27 @@ class PandasCalculator(object):
         super(PandasCalculator, self).__init__()
 
     def median(self, pandas_series):
-        return pandas_series.median()
+        return pandas_series.series.median()
+
+    def mean(self, pandas_series):
+        return pandas_series.series.mean()
+
+    def sum(self, pandas_series):
+        return pandas_series.series.sum()
+
+    def square_diff(self, pandas_series1, pandas_series2):
+        new_ps = pandas_series1.make_copy()
+        new_ps.series = (pandas_series1.series - pandas_series2.series)**2
+        return new_ps
+
+    def abs_diff(self, pandas_series1, pandas_series2):
+        new_ps = pandas_series1.make_copy()
+        diff_ts = (pandas_series1.series - pandas_series2.series)
+        new_ps.series = diff_ts.abs()
+        return new_ps
+
+    def min(self, pandas_series):
+        return pandas_series.series.min()
+
+    def argmin(self, pandas_series):
+        return pandas_series.series.argmin()
