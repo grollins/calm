9f06e1cde19bcc7fa14649f52abf9c39e24ff55f
Geoff Rollins <grollins@gmail.com>
2013-06-25 13:23:05 -0700

    Implement methods for getting and setting time series values.

--- calm/pandas_time_series.py
+++ calm/pandas_time_series.py
@@ -1,34 +1,51 @@
+import numpy
 import pandas
 
 class PandasTimeSeries(object):
     """docstring for PandasTimeSeries"""
     def __init__(self):
         super(PandasTimeSeries, self).__init__()
-        self.time_series = None
+        self.series = None
 
     def __len__(self):
-        return len(self.time_series)
+        return len(self.series)
 
     def __str__(self):
-        return str(self.time_series)
+        return str(self.series)
 
     def load_csv(self, csv_path):
-        self.time_series = pandas.read_csv(csv_path, index_col=0, header=None,
+        self.series = pandas.read_csv(csv_path, index_col=0, header=None,
                                            squeeze=True)
 
     def write_csv(self, csv_path):
-        self.time_series.to_csv(csv_path, header=False)
+        self.series.to_csv(csv_path, header=False)
 
     def load_npy_arrays(self, time_array, signal_array):
-        self.time_series = pandas.Series(signal_array, index=time_array)
+        self.series = pandas.Series(signal_array, index=time_array)
 
     def make_copy(self):
         my_copy = PandasTimeSeries()
-        my_copy.time_series = self.time_series.copy()
+        my_copy.series = self.series.copy()
         return my_copy
 
-    def set_signal_from_scalar(self, scalar):
-        self.time_series.fill(scalar)
+    def set_signal_from_scalar(self, scalar, index_list=None):
+        if index_list:
+            scalar_list = [scalar] * len(index_list)
+            self.set_values_at_indices(scalar_list, index_list)
+        else:
+            self.series.fill(scalar)
 
-    def median(self):
-        return self.time_series.median()
+    def is_similar_to(self, other_time_series, atol=1e-1):
+        return numpy.allclose(self.series, other_time_series.series, atol=atol)
+
+    def get_values_at_indices(self, index_list):
+        sub_series = self.series.ix[index_list]
+        sub_ts = PandasTimeSeries()
+        sub_ts.series = sub_series
+        return sub_ts
+
+    def set_values_at_indices(self, new_values, index_list):
+        self.series.ix[index_list] = new_values
+
+    def isfinite(self):
+        return numpy.isfinite(self.series).all()
