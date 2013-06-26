909cc79f058c9904a0c6733d6e3b32206bf5b03a
Geoff Rollins <grollins@gmail.com>
2013-06-25 21:41:45 -0700

    Refactor jump penalty denoiser so that variable names are clearer.

--- calm/jump_penalty_denoiser.py
+++ calm/jump_penalty_denoiser.py
@@ -1,3 +1,16 @@
+"""
+Implements the least-squares and least-absolute jump penalization methods
+described by Little and Jones:
+Little, M. A., and Jones, N. S. Generalized methods and solvers for noise
+removal from piecewise constant signals. II. New methods.
+Proc. R. Soc. A-Math. Phys. Eng. Sci. 467, 2135 (2011), 3115-3140.
+
+Input arguments:
+square     `True` perform least-squares fitting,
+           `False` perform least-absolute (robust) fitting.
+gamma      Positive regularization parameter.
+"""
+
 import numpy
 from copy import copy
 
@@ -7,185 +20,130 @@ class JumpPenaltyDenoiser(object):
         super(JumpPenaltyDenoiser, self).__init__()
         self.calculator = calculator
 
-    def run_denoising(self, time_series, square, gamma, noisy=False):
-        N = len(time_series)
-        # r = zeros(0,1) # Knot locations
-        r = []
+    def run_denoising(self, raw_ts, square, gamma, noisy=False):
+        N = len(raw_ts)
+        knot_list = []
 
-        # Eold = Inf
-        Eold = 1e99
+        H_old = 1e99
 
         if noisy:
             print 'Iter# Global functional'
 
-        # Iterate
         iter_num = 1
         maxiter = 50
         while iter_num < maxiter:
-
             if noisy:
-                print '%5d %7.2e' % (iter_num, Eold)
-
-            # Greedy scan for new knot location
-            # NLL = zeros(N,1);
+                print '%5d %7.2e' % (iter_num, H_old)
+            # =====================================
+            # = Greedy scan for new knot location =
+            # =====================================
             NLL = numpy.zeros(N)
-
-            # for i = 1:N
             for i in xrange(N):
                 # Append new location
-                # r1 = r;
-                # r1(end+1) = i;
-                # r1 = sort(r1);
-                r1 = copy(r)
-                r1.append(i)
-                r1.sort()
-
+                trial_knot_list = copy(knot_list)
+                trial_knot_list.append(i)
+                trial_knot_list.sort()
                 # Find optimum levels
-                this_lh = self.find_optimum_levels(time_series, square, r1,
-                                                   iter_num, N)
+                this_lh = self.find_optimum_levels(
+                            raw_ts, square, trial_knot_list, iter_num, N)
                 NLL[i] = this_lh
-
-            # Choose new location that minimizes the likelihood term
-            # [v,i] = min(NLL);
+                # end for
+            # ==========================================================
+            # = Choose new location that minimizes the likelihood term =
+            # ==========================================================
             min_val = numpy.min(NLL)
             i = numpy.argmin(NLL)
-
             # Add to knot locations
-            # r(end+1) = i;
-            r.append(i)
-            # r = sort(r);
-            r.sort()
-
+            knot_list.append(i)
+            knot_list.sort()
             # Re-compute solution at this iteration
-            Enew, xnew = self.compute_solution(time_series, square, gamma,
-                                                 r, iter_num, N)
-
-            # Detect local minima in global functional
-            if Eold < Enew:
+            H_new, smooth_ts = self.compute_solution(raw_ts, square, gamma,
+                                                     knot_list, iter_num, N)
+            # =========================
+            # = Check for convergence =
+            # =========================
+            if H_old < H_new:
                 if noisy:
                     print 'Converged in %d iterations' % iter_num
-                break
-
-            Eold = Enew;
-            iter_num += 1
+                break # stop while loop
+            else:
+                H_old = H_new;
+                iter_num += 1
+            # end while
 
         if noisy:
             if iter_num == maxiter:
                 print 'Maximum iterations exceeded'
+        return smooth_ts
 
-        x = xnew
-        E = Enew
-        return x
+    def compute_global_fcn(self, m, x, square, gamma, iter_num):
+        H = self.compute_likelihood(m, x, square)
+        H += gamma * iter_num
+        return H
 
-    def compute_global_fcn(self, x, y, square, gamma, iter_num):
-        E = self.compute_likelihood(x, y, square)
-        E += gamma * iter_num
-        # if square:
-            # E = 0.5 * self.calculator.sum((x-y)**2) + gamma*iter_num
-        # else:
-            # E = self.calculator.sum(self.calculator.abs(x-y)) + gamma*iter_num
-        return E
-
-    def compute_likelihood(self, x, y, square):
+    def compute_likelihood(self, m, x, square):
         if square:
-            # likelihood = 0.5 * self.calculator.sum((x-y)**2)
-            likelihood = self.calculator.sum(self.calculator.square_diff(x, y))
+            likelihood = self.calculator.sum(self.calculator.square_diff(m, x))
             likelihood *= 0.5
         else:
-            # likelihood = self.calculator.sum(self.calculator.abs(x-y))
-            likelihood = self.calculator.sum(self.calculator.abs_diff(x, y))
+            likelihood = self.calculator.sum(self.calculator.abs_diff(m, x))
         return likelihood
 
-    def mean_or_median(self, y, square):
-        if len(y) == 0:
-            m = numpy.nan
+    def compute_level(self, x, square):
+        if len(x) == 0:
+            level = numpy.nan
         else:
             if square:
-                m = self.calculator.mean(y)
+                level = self.calculator.mean(x)
             else:
-                m = self.calculator.median(y)
-        return m
+                level = self.calculator.median(x)
+        return level
 
-    def find_optimum_levels(self, time_series, square, r1, iter_num, N):
-        # xtest = zeros(N,1);
-        xtest = time_series.make_copy()
-        xtest.set_signal_from_scalar(0.0)
-        assert xtest.isfinite(), str(xtest)
-        # L = 1:(r1(1)-1);
-        upper_limit = r1[0]-1
+    def find_optimum_levels(self, raw_ts, square, knot_list, iter_num, N):
+        smooth_ts = raw_ts.make_copy()
+        smooth_ts.set_signal_from_scalar(0.0)
+        upper_limit = knot_list[0]-1
         L = range(upper_limit)
         if len(L) > 0:
-            # xtest(L) = mean_or_median(y(L), square);
-            mean_or_median = self.mean_or_median(
-                                time_series.get_values_at_indices(L),
-                                square)
-            xtest.set_signal_from_scalar(mean_or_median, L)
-            assert xtest.isfinite(), str(L)
+            level = self.compute_level(raw_ts.get_values_at_indices(L), square)
+            smooth_ts.set_signal_from_scalar(level, L)
+            assert smooth_ts.isfinite(), str(L)
         else:
             pass
-        # for j = 2:iter
         for j in xrange(1, iter_num):
-            # L = r1(j-1):(r1(j)-1);
-            L = range(r1[j-1], r1[j])
+            L = range(knot_list[j-1], knot_list[j])
             if len(L) > 0:
-                # xtest(L) = mean_or_median(y(L), square);
-                mean_or_median = self.mean_or_median(
-                                    time_series.get_values_at_indices(L),
-                                    square)
-                xtest.set_signal_from_scalar(mean_or_median, L)
-                assert xtest.isfinite(), str(xtest)
+                level = self.compute_level(
+                            raw_ts.get_values_at_indices(L), square)
+                smooth_ts.set_signal_from_scalar(level, L)
+                assert smooth_ts.isfinite(), str(smooth_ts)
             else:
                 pass
-
-        # L = r1(iter):N;
-        L = range(r1[iter_num-1], N)
-        # xtest(L) = mean_or_median(y(L), square);
+        L = range(knot_list[iter_num-1], N)
         if len(L) > 0:
-            mean_or_median = self.mean_or_median(
-                                time_series.get_values_at_indices(L),
-                                square)
-            xtest.set_signal_from_scalar(mean_or_median, L)
-            assert xtest.isfinite(), str(xtest)
+            level = self.compute_level(raw_ts.get_values_at_indices(L), square)
+            smooth_ts.set_signal_from_scalar(level, L)
+            assert smooth_ts.isfinite(), str(smooth_ts)
         else:
             pass
-
         # Compute likelihood
-        # NLL(i) = compute_likelihood(xtest, y, square);
-        this_lh = self.compute_likelihood(xtest, time_series, square)
-        assert numpy.isfinite(this_lh), str(xtest)
-        assert xtest.isfinite(), str(xtest)
+        this_lh = self.compute_likelihood(smooth_ts, raw_ts, square)
         return this_lh
 
-    def compute_solution(self, time_series, square, gamma, r, iter_num, N):
-        # i = 1:(r(1)-1);
-        i = range(r[0]-1)
-        # xnew = zeros(N,1);
-        xnew = time_series.make_copy()
-        xnew.set_signal_from_scalar(0.0)
-        # xnew(i) = mean_or_median(y(i), square);
-        mean_or_median = self.mean_or_median(
-                            time_series.get_values_at_indices(i), square)
-        xnew.set_signal_from_scalar(mean_or_median, i)
-        # for j = 2:iter
+    def compute_solution(self, raw_ts, square, gamma, knot_list, iter_num, N):
+        i = range(0, knot_list[0]-1)
+        smooth_ts = raw_ts.make_copy()
+        smooth_ts.set_signal_from_scalar(0.0)
+        level = self.compute_level(raw_ts.get_values_at_indices(i), square)
+        smooth_ts.set_signal_from_scalar(level, i)
         for j in xrange(1, iter_num):
-            # i = r(j-1):(r(j)-1);
-            i = range(r[j-1], r[j])
-            # xnew(i) = mean_or_median(y(i), square);
-            mean_or_median = self.mean_or_median(
-                                time_series.get_values_at_indices(i),
-                                square)
-            xnew.set_signal_from_scalar(mean_or_median, i)
-
-        # i = r(iter):N;
-        i = range(r[iter_num-1], N)
-        # xnew(i) = mean_or_median(y(i), square);
-        mean_or_median = self.mean_or_median(
-                            time_series.get_values_at_indices(i), square)
-        xnew.set_signal_from_scalar(mean_or_median, i)
-
+            i = range(knot_list[j-1], knot_list[j])
+            level = self.compute_level(raw_ts.get_values_at_indices(i), square)
+            smooth_ts.set_signal_from_scalar(level, i)
+        i = range(knot_list[iter_num-1], N)
+        level = self.compute_level(raw_ts.get_values_at_indices(i), square)
+        smooth_ts.set_signal_from_scalar(level, i)
         # Compute global functional value at current solution
-        # Enew = compute_global_fcn(xnew, y, square, gamma, iter);
-        Enew = self.compute_global_fcn(xnew, time_series, square, gamma,
-                                       iter_num)
-        return Enew, xnew
+        H = self.compute_global_fcn(smooth_ts, raw_ts, square, gamma, iter_num)
+        return H, smooth_ts
 
